478b80a410276f3d0f306a804a24f91f73e40363
Koki <hukasan27@gmail.com>
2020-08-05 00:53:18 +0900

    Added:入退室処理追加

--- Cogs/EventCog.py
+++ Cogs/EventCog.py
@@ -1,8 +1,7 @@
-import discord
+from discord import Embed, Member, AuditLogAction
 from discord.ext import commands
-from datetime import datetime, timezone
-from pytz import timezone, utc
-# from tzlocal import get_localzone
+from datetime import datetime
+from pytz import utc
 
 
 class Event(commands.Cog):
@@ -13,20 +12,47 @@ class Event(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.lastchecktime = (datetime.now(utc))
-        self.room_id = (self.bot.config['wkwm']['room_id'])
+        self.room_id = int(self.bot.config['wkwm']['room_id'])
+        self.role_nozoki_id = int(self.bot.config['wkwm']['role_nozoki_id'])
+        self.welcome_room_id = int(self.bot.config['wkwm']['welcome_room_id'])
+        self.welcome_message = str(self.bot.config['wkwm']['welcome_message'])
+        self.leave_notice_room_id = int(
+            self.bot.config['wkwm']['leave_notice_room_id'])
+
+    def default_embed(self, message):
+        return Embed.from_dict({
+            'description': message,
+            'color': 0x00ff00,
+        })
+
+    @ commands.Cog.listener()
+    async def on_member_join(self, member: Member):
+        if member.bot:
+            return
+        role_member = member.guild.get_role(self.role_nozoki_id)
+        await member.add_roles(role_member)
+        welcome_room = self.bot.get_channel(self.welcome_room_id)
+        await welcome_room.send(embed=self.default_embed(f"ようこそ猿sのばなな農園へ🍌\r{member.mention}さん\r{self.welcome_message}"))
 
     @commands.Cog.listener()
-    async def on_member_update(self, before: discord.Member, after: discord.Member):
-        self.room = self.bot.get_channel(int(self.room_id))
+    async def on_member_remove(self, member: Member):
+        if member.bot:
+            return
+        leave_notice_room = self.bot.get_channel(self.leave_notice_room_id)
+        await leave_notice_room.send(embed=self.default_embed(f" **{member.name}** が脱退しました \rUserID: {member.mention}"))
+
+    @ commands.Cog.listener()
+    async def on_member_update(self, before: Member, after: Member):
+        self.room = self.bot.get_channel(self.room_id)
         br = set(before.roles)
         ar = set(after.roles)
         dif = len(br) - len(ar)
         if dif != 0:
             conf = list((br-ar) if len(br) > len(ar) else (ar-br))
             # print(f"__________{self.lastchecktime}____________")
-            async for entry in after.guild.audit_logs(action=discord.AuditLogAction.member_role_update, oldest_first=False):
+            async for entry in after.guild.audit_logs(action=AuditLogAction.member_role_update, oldest_first=False):
                 if entry.created_at > self.lastchecktime.replace(tzinfo=None):
-                    if isinstance(entry.target, discord.Member):
+                    if isinstance(entry.target, Member):
                         if dif > 0:
                             conf = list(br-ar)
                             await self.room.send(f'{entry.user.mention} は {entry.target.mention} から **{conf[0].name}** のロールを抜きました')
