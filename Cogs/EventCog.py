089d7e9c9bd27569dcb459a9cd362f7abce2bca2
Koki <hukasan27@gmail.com>
2020-08-08 16:29:08 +0900

    ロール変更通知がうループ外で入らなくなる不具合を修正(というかwelcomemessageのエラー)

--- Cogs/EventCog.py
+++ Cogs/EventCog.py
@@ -1,4 +1,4 @@
-from discord import Embed, Member, AuditLogAction
+from discord import Embed, Member, AuditLogAction, User
 from discord.ext import commands
 from datetime import datetime
 from pytz import utc
@@ -27,7 +27,7 @@ class Event(commands.Cog):
         role_member = member.guild.get_role(self.role_nozoki_id)
         await member.add_roles(role_member)
         welcome_room = self.bot.get_channel(self.welcome_room_id)
-        await welcome_room.send(embed=Option.default_embed(description=f"ようこそ猿sのばなな農園へ🍌\r{member.mention}さん\r{self.welcome_message}"))
+        await welcome_room.send(embed=await Option.default_embed(description=f"ようこそ猿sのばなな農園へ🍌\r{member.mention}さん\r{self.welcome_message}"))
 
     @ commands.Cog.listener()
     async def on_member_remove(self, member: Member):
@@ -37,25 +37,23 @@ class Event(commands.Cog):
         await leave_notice_room.send(embed=Option.default_embed(description=f" **{member.name}** が脱退しました \rUserID: {member.mention}"))
 
     @ commands.Cog.listener()
-    async def on_member_update(self, before: Member, after: Member):
+    async def on_member_update(self, before, after):
         self.room = self.bot.get_channel(self.room_id)
         br = set(before.roles)
         ar = set(after.roles)
         dif = len(br) - len(ar)
         if dif != 0:
-            conf = list((br-ar) if len(br) > len(ar) else (ar-br))
+            conf = list((br - ar) if len(br) > len(ar) else (ar - br))
             # print(f"__________{self.lastchecktime}____________")
             async for entry in after.guild.audit_logs(action=AuditLogAction.member_role_update, oldest_first=False):
                 if entry.created_at > self.lastchecktime.replace(tzinfo=None):
-                    if isinstance(entry.target, Member):
+                    if isinstance(entry.target, Member) | isinstance(entry.target, User):
                         if dif > 0:
                             conf = list(br-ar)
-                            await self.room.send(f'{entry.user.mention} は {entry.target.mention} から **{conf[0].name}** のロールを抜きました')
-
+                            await self.room.send(f'{entry.user.mention} は {after.mention} から **{conf[0].name}** のロールを抜きました')
                         elif dif < 0:
                             conf = list(ar - br)
-                            await self.room.send(f'{entry.user.mention} が {entry.target.mention} に **{conf[0].name}** のロールを与え給いました')
-
+                            await self.room.send(f'{entry.user.mention} が {after.mention} に **{conf[0].name}** のロールを与え給いました')
         self.lastchecktime = (datetime.now(utc))
 
 
