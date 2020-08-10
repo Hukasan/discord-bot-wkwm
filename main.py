1b719612c46d483d2b0e3009f5357a6293763c41
Koki <hukasan27@gmail.com>
2020-08-10 20:54:00 +0900

    help改善

--- main.py
+++ main.py
@@ -17,15 +17,25 @@ extensions.append("ReactionEvent")
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    bot = commands.Bot(command_prefix="$", case_insensitive=True,
-                       description="Saru's Wakewakaran Bot Project")
+    bot = commands.Bot(
+        command_prefix=[
+            '$',
+            '＄',
+            '?',
+            '？'],
+        case_insensitive=True,
+        description="Saru's Wakewakaran Bot Project")
+    # config["wkwm"]["room_id"] = 742001514771382313
+    # config['wkwm']['welcome_room_id'] = 742001514771382313
+    # config['wkwm']['leave_notice_room_id'] = 742001514771382313
+    # config['wkwm']['nozoki_role_id'] = 712526833995743252
+    # TOKEN = "NzQyMDA0MDk5MjU1NjMxOTA0.Xy_zKw.rRCRiqoZcWnqQy9h3pajl8KWLzs"
     config["wkwm"]["room_id"] = environ['BOT_ROOM_ID']
     config['wkwm']['welcome_room_id'] = environ['WELCOME_ROOM_ID']
     config['wkwm']['leave_notice_room_id'] = environ['LEAVE_NOTICE_ROOM_ID']
     config['wkwm']['nozoki_role_id'] = environ['NOZOKI_ROLE_ID']
-
-    bot.config = config
     TOKEN = environ['BOT_ACCESS_TOKEN']  # 環境変数から取得
+    bot.config = config
     for extension in extensions:
         bot.load_extension(f"Cogs.{extension}")
     bot.run(TOKEN)
