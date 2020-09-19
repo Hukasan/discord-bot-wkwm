6916fcd99fc4c408feeebf14035b872e1c189266
Koki <hukasan27@gmail.com>
2020-09-19 23:18:34 +0900

    リアクションのデバッグメッセージ削除

--- Cogs/reaction_cog.py
+++ Cogs/reaction_cog.py
@@ -35,14 +35,12 @@ class ReactionEvent(Cog):
 
     @ Cog.listener()
     async def on_raw_reaction_add(self, rrae: RawReactionActionEvent):
-        print("リアクション検知")
         usr = self.bot.get_user(rrae.user_id)
         channel = TextChannel
         channel = self.bot.get_channel(rrae.channel_id)
         emoji = self.bot.get_emoji(rrae.emoji.id)
         if bool(channel) & bool(emoji) & bool(usr):
             if not(usr.bot):
-                print("正規なリアクション")
                 message = Message
                 message = await channel.fetch_message(rrae.message_id)
                 if message.embeds:
