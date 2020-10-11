a7a750eb068fa24adb6e97aebfae933aae4e7266
Koki <hukasan27@gmail.com>
2020-10-11 21:15:31 +0900

    無駄文除去

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
 from os import environ, listdir, path
 from discord.ext.commands import Bot
 
-# import logging
-# logging.basicConfig(filename='log/logger.log', level=logging.ERROR)
-# logger = logging.getLogger(__name__)
 
-config = {
-    "wkwm": {"welcome_message": ["入隊希望などの申請は各チャンネルへお願いします。", "各チャンネルのピン留めに募集詳細が有ります。"]}
-}
-
-p = "Cogs"
-files = listdir(p)
+files = listdir("Cogs")
 extensions = [path.splitext(f)[0] for f in files if path.isfile(path.join(p, f))]
 if __name__ == "__main__":
-    # logging.basicConfig(level=logging.INFO)
     bot = Bot(
         command_prefix=["$", "＄", "?", "？"],
         description="Saru's Wakewakaran Bot Project",
     )
+    config = {}
+    config["wkwm"] = {}
     config["wkwm"]["room_id"] = environ["BOT_ROOM_ID"]
     config["wkwm"]["welcome_room_id"] = environ["WELCOME_ROOM_ID"]
     config["wkwm"]["leave_notice_room_id"] = environ["LEAVE_NOTICE_ROOM_ID"]
