6c3f6791982a356aca598cb40efa352ecf997b02
Koki <hukasan27@gmail.com>
2020-10-18 21:11:38 +0900

    なんでロール取得できないかちょっとわかんないからデバッグ

--- Cogs/app/role_checker.py
+++ Cogs/app/role_checker.py
@@ -25,6 +25,7 @@ async def isroleupper(role_id: int, user: User, ignore_same=True) -> bool:
         guild_role_list = list()
         guild_role_list = await guild.fetch_roles()
         comp_index = guild_role_list.index(comp_role)
+        print(comp_index, guild_role_list.index(member.top_role))
         if (comp_index >= guild_role_list.index(member.top_role)) & (bool(ignore_same)):
             return True
         elif (comp_index > guild_role_list.index(member.top_role)) & (
